c4b00aad613bd735983056c796df303c8a79bd5c
hilal555 <muhamadhilal04@gmail.com>
2023-06-23 09:33:59 +0700

    cek antrian kosong

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -22,5 +22,19 @@ public:
 			cout << "\nQueue overFLow\n";
 			return;
 		}
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika rear berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
 };
